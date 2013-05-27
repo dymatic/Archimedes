c8e9f1fbe6951372ada59cc5d9efbcb87759255a
Norton Jenkins <dominantshoe@gmail.com>
2013-05-27 01:39:50 -0400

    Update library

--- Build.sh
+++ Build.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 rm -rf ~/Code/Haskell/Macro/Archimedes
 cp -R ./Archimedes ~/Code/Haskell/Macro/Archimedes
+
+rm -rf ~/Code/Haskell/Oak/Archimedes
+cp -R ./Archimedes ~/Code/Haskell/Macro/Archimedes
